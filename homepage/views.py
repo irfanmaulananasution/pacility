64c4fd7300d1858f7ba412e798ed27e933788de3
Tsaqif <tsaqifnaufal91@gmail.com>
2019-12-05 23:32:15 +0700

    [GREEN] pass context dictionary to render argument

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import AnnouncementForm
+from .models import Announcement
+from datetime import datetime, timedelta
 
 def index(request):
-	return render(request, 'homepage/homepage.html')
+	context = {
+		"form": AnnouncementForm(),
+		"announcements": Announcement.objects.all()
+	}
+
+	return render(request, 'homepage/homepage.html', context)
