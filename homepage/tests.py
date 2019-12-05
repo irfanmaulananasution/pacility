11ad0ae222c2425484c316efe3e76e605b33cbb4
Tsaqif <tsaqifnaufal91@gmail.com>
2019-12-05 23:03:16 +0700

    [GREEN] add setUp function

--- homepage/tests.py
+++ homepage/tests.py
@@ -4,6 +4,24 @@ from .views import index
 from .models import Announcement
 
 class AnnouncementTestCase(TestCase):
+	def setUp(self):
+		username1 = "louise.banks"
+		initial1 = "L"
+		title1 = "Announcement1"
+		content1 = "Announcement content goes here..."
+		date1 = "Saturday, 19 October 2019"
+		time1 = "10:35"
+
+		username2 = "ian.donnelly"
+		initial2 = "I"
+		title2 = "Announcement2"
+		content2 = "Another announcement content..."
+		date2 = "Saturday, 19 October 2019"
+		time2 = "18:43"
+
+		Announcement.objects.create(username = username1, initial = initial1, title = title1, content = content1, date = date1, time = time1)
+		Announcement.objects.create(username = username2, initial = initial2, title = title2, content = content2, date = date2, time = time2)
+
 	def test_url_index_exists(self):
 		response = self.client.get('/')
 		self.assertEqual(response.status_code, 200)
@@ -16,17 +34,17 @@ class AnnouncementTestCase(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'homepage/homepage.html')
 
+	def test_announcement_count(self):
+		announcements = Announcement.objects.all()
+		self.assertEqual(announcements.count(), 2)
+
 	def test_valid_announcement(self):
-		username = "louise.banks"
-		initial = "L"
-		title = "Announcement1"
-		content = "Announcement content goes here..."
-		date = "Saturday, 19 October 2019"
-		time = "10:35"
+		announcement1 = Announcement.objects.get(id=1)
+		announcement2 = Announcement.objects.get(id=2)
 
-		announcement = Announcement.objects.create(username = username, initial = initial, title = title, content = content, date = date, time = time)
-		self.assertTrue(announcement.is_valid_announcement())
-
+		self.assertTrue(announcement1.is_valid_announcement())
+		self.assertTrue(announcement2.is_valid_announcement())
+	
 	def test_invalid_username(self):
 		username = "floccinaucinihilipilification"
 		initial = "F"
@@ -59,3 +77,4 @@ class AnnouncementTestCase(TestCase):
 
 		announcement = Announcement.objects.create(username = username, initial = initial, title = title, content = content, date = date, time = time)
 		self.assertFalse(announcement.is_valid_announcement())
+
