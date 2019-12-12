eac15a01d019cc604decc66d4895fd1c6ae0167a
Tsaqif <tsaqifnaufal91@gmail.com>
2019-12-12 16:42:33 +0700

    [GREEN] create url and view function for login

--- base/views.py
+++ base/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
-# Create your views here.
+def login_view(request):
+	if request.method == "POST":
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return HttpResponse('Success')
+		return HttpResponse('Not Found')
+
+	return HttpResponseRedirect(reverse('homepage:index'))
