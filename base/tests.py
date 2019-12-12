1a31855b63e49b203b2514555fa292ba4913b793
Tsaqif <tsaqifnaufal91@gmail.com>
2019-12-12 16:15:48 +0700

    [RED] create tests for login url and view function

--- base/tests.py
+++ base/tests.py
@@ -1,74 +1,34 @@
-from django.test import TestCase, Client
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+from django.test import TestCase
+from django.urls import resolve
+from .views import login_view
+from django.contrib.auth.models import User
 
-#self.browser = webdriver.Chrome(chrome_options=options, executable_path="base/chrome_77_driver/chromedriver")
-# class PacilityFunctionalTest(TestCase):
-# 	def setUp(self):
-# 		self.client = Client()
-# 		chrome_options = Options()
-# 		chrome_options.add_argument('--dns-prefetch-disable')
-# 		chrome_options.add_argument('--no-sandbox')
-# 		chrome_options.add_argument('--headless')
-# 		chrome_options.add_argument('disable-gpu')
+class BaseTestCase(TestCase):
+	def setUp(self):
+		first_name = 'Louise'
+		last_name = 'Banks'
+		username = 'louise.banks'
+		email = 'louisebanks@arrival.com'
+		password = 'arrival'
+		new_user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+		new_user.save()
 
-# 		self.selenium  = webdriver.Chrome(chrome_options = chrome_options)
-# 		super(PacilityFunctionalTest, self).setUp()
+	def test_url_login_if_get_method(self):
+		response = self.client.get('/base/login')
+		self.assertEqual(response.status_code, 302)
 
-# 	def tearDown(self):
-# 		self.selenium.quit()
-# 		super(PacilityFunctionalTest, self).tearDown()
+	def test_url_login_if_post_and_auth_failed(self):
+		response = self.client.post('/base/login', data={'username': 'ian.donnelly', 'password': 'arrival'})
+		html_response = response.content.decode('utf8')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('Not Found', html_response)
 
-# 	def test_functionality(self):
-# 		# selenium = self.selenium
-# 		self.selenium.get('pacility.herokuapp.com/')
-# 		time.sleep(3)
+	def test_url_login_if_post_and_auth_success(self):
+		response = self.client.post('/base/login', data={'username': 'louise.banks', 'password': 'arrival'})
+		html_response = response.content.decode('utf8')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('Success', html_response)
 
-# 		self.selenium.execute_script("window.scrollTo(0, 500)")
-# 		time.sleep(3)
-
-# 		username = self.selenium.find_element_by_id('id_username')
-# 		title = self.selenium.find_element_by_id('id_title')
-# 		content = self.selenium.find_element_by_id('id_content')
-# 		button1 = self.selenium.find_element_by_class_name('btn-submit')
-
-# 		username.send_keys('tsqfnfl')
-# 		time.sleep(3)
-# 		title.send_keys('Tes Fungsionalitas Homepage')
-# 		time.sleep(3)
-# 		content.send_keys('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis trud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat la pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
-# 		time.sleep(3)
-# 		button1.click()
-# 		time.sleep(3)
-
-# 		self.selenium.execute_script("window.scrollTo(0, 500)")
-# 		time.sleep(1)
-# 		self.selenium.execute_script("window.scrollTo(500, 900)")
-# 		time.sleep(3)
-
-# 		form_nav = self.selenium.find_element_by_id('nav-form')
-# 		form_nav.click()
-# 		time.sleep(3)
-
-# 		title = self.selenium.find_element_by_id('id_form_title')
-# 		location = self.selenium.find_element_by_id('id_location')
-# 		time_input = self.selenium.find_element_by_id('id_time')
-# 		category = self.selenium.find_element_by_id('id_category')
-# 		description = self.selenium.find_element_by_id('id_description')
-# 		button2 = self.selenium.find_element_by_class_name('btn-submit')
-
-# 		title.send_keys('Kerusakan Meja Kantin')
-# 		time.sleep(3)
-# 		location.send_keys('Kantin Fasilkom')
-# 		time.sleep(3)
-# 		time_input.send_keys('900A')
-# 		time.sleep(3)
-# 		category.send_keys('Fasilkom')
-# 		time.sleep(3)
-# 		description.send_keys('Kaki meja patah, lokasi meja di dekat lorong antara gedung B dan gedung C.')
-# 		time.sleep(3)
-# 		selenium.execute_script("window.scrollTo(0, 500)")
-# 		time.sleep(1)
-# 		button2.click()
-# 		time.sleep(5)
+	def test_using_login_view_function(self):
+		found = resolve('/base/login')
+		self.assertEqual(found.func, login_view)
