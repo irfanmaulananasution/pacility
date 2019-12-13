c2b3fffd1dffcca25bd3cc0d31aaea502ddda5d7
Timothy Regana Tarigan <timothyregana@gmail.com>
2019-12-13 16:13:53 +0700

    added scripts for logged in users

--- report_list/views.py
+++ report_list/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from datetime import datetime
 from report_form.models import Report
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def report_list(request):
 	reports = Report.objects.all()
-	return render(request, 'report_list/report_list.html', {'reports': reports})
+	return render(request, 'report_list/report_list.html', {'reports': reports, 'user':request.user})
 
 
